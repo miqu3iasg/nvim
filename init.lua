4504f7b62cb7b813868a49ef5c449050bc365075
miqu3iasg <147787560+miqu3iasg@users.noreply.github.com>
2026-09-24 19:42:50 -0300

    chore(theme): change default theme to nox

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ require("statusline")
 
 utils.color_overrides.setup_colorscheme_overrides()
 
-vim.cmd.colorscheme("insanity")
+-- vim.cmd.colorscheme("insanity")
+vim.cmd.colorscheme("nox")
 
 utils.fix_telescope_parens_win()
